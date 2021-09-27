18db33cfae6414345db34387a86d9ae644c53ba1
pastepi <pawel.st123@gmail.com>
2021-09-27 20:18:22 +0200

    Adding material UI

--- frontend/src/URLShortener.js
+++ frontend/src/URLShortener.js
@@ -1,5 +1,5 @@
 import { useState } from "react"
-
+import { Input, Button, Link, Alert, FormGroup, Box } from '@mui/material'
 
 const defaultContainerStyle = {minHeight: "150px", margin: "0", minWidth: "300px", display: "flex", alignItems: "center", justifyContent: "center"};
 const defaultFormStyle = {display: "flex", flexDirection: "column", alignItems: "center", minHeight: "150px", justifyContent: "space-around"};
@@ -12,7 +12,7 @@ const defaultErrorStyle = {color: "red"};
 const myHost = process.env.REACT_APP_BACKEND_URL;
 
 
-const URLShortener = ({ containerStyle, formStyle, buttonStyle, inputStyle, linkStyle, errorStyle }) => {
+const URLShortener = ({ containerStyle, formStyle, buttonStyle, inputStyle, linkStyle, errorStyle, useMaterialUI }) => {
     
     const [url, setURL] = useState("");
     const [shortURL, setShortURL] = useState(null);
@@ -34,34 +34,36 @@ const URLShortener = ({ containerStyle, formStyle, buttonStyle, inputStyle, link
     }
 
     const handleButtonClick = (e) => {
-      e.preventDefault()
-      if (error) {
-        setErrorState(false)
-      }
-
-      if (isValidURL(url)) {
-        let srcURL = url;
-
-        // Checks for the scheme/protocol of the link 
-        // - if no valid one exists, adds "HTTPS"
-        try {
-          new URL(srcURL);
-        } catch (_) { // MalformedURLException
-          srcURL = "https://" + srcURL;
-          setURL(srcURL);
+      if (e.keyCode === 13 || e.keyCode === undefined) {
+        e.preventDefault()
+        if (error) {
+          setErrorState(false)
         }
-
-        fetch(myHost + "/URL", {
-          method: "POST", 
-          headers: {
-          "Content-Type": "application/json"
-          },
-          body: JSON.stringify({url:srcURL})
-      }).then(response => response.json()
-      ).then(data => setShortURL(data.ShortURL))
-      } else {
-        setErrorState(true);
-        setShortURL(null);
+    
+        if (isValidURL(url)) {
+          let srcURL = url;
+    
+          // Checks for the scheme/protocol of the link 
+          // - if no valid one exists, adds "HTTPS"
+          try {
+            new URL(srcURL);
+          } catch (_) { // MalformedURLException
+            srcURL = "https://" + srcURL;
+            setURL(srcURL);
+          }
+    
+          fetch(myHost + "/URL", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ url: srcURL })
+          }).then(response => response.json()
+          ).then(data => setShortURL(data.ShortURL))
+        } else {
+          setErrorState(true);
+          setShortURL(null);
+        }
       }
     }
 
@@ -69,16 +71,24 @@ const URLShortener = ({ containerStyle, formStyle, buttonStyle, inputStyle, link
       return style ? {...defStyle, ...style} : defStyle
     }
     
-    return (
-      <div style={ styleElements(defaultContainerStyle, containerStyle) }>
-        <form style={ styleElements(defaultFormStyle, formStyle) }>
-          <input          style={ styleElements(defaultInputStyle, inputStyle) } type="text" value={url} onChange={handleURLChange} spellCheck="false" placeholder="Your URL"/>
-          <button         style={ styleElements(defaultButtonStyle, buttonStyle) } onClick={handleButtonClick}>Shorten URL</button>
-          {shortURL && <a style={ styleElements(defaultLinkStyle, linkStyle) } href={`${myHost}/${shortURL}`} rel="noreferrer" target="_blank" >{`${myHost}/${shortURL}`}</a>}
-          {error && <div  style={ styleElements(defaultErrorStyle, errorStyle) }>URL is not valid</div>}
+    return !useMaterialUI ? (
+      <div style={styleElements(defaultContainerStyle, containerStyle)}>
+        <form style={styleElements(defaultFormStyle, formStyle)}>
+          <input style={styleElements(defaultInputStyle, inputStyle)} type="text" value={url} onChange={handleURLChange} spellCheck="false" placeholder="Your URL" />
+          <button style={styleElements(defaultButtonStyle, buttonStyle)} onClick={handleButtonClick}>Shorten URL</button>
+          {shortURL && <a style={styleElements(defaultLinkStyle, linkStyle)} href={`${myHost}/${shortURL}`} rel="noreferrer" target="_blank" >{`${myHost}/${shortURL}`}</a>}
+          {error && <div style={styleElements(defaultErrorStyle, errorStyle)}>URL is not valid</div>}
         </form>
       </div>
-    );
-}
+    ) : (<div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+      <Box sx={{ border: 1 }} style={{ width: "80%", maxWidth: "500px", padding: "16px", borderRadius: "5px", border: "2px solid #1976d2" }} >
+      <FormGroup>
+        <Input type="text" onKeyDown={handleButtonClick} value={url} onChange={handleURLChange} spellCheck="false" placeholder="Your URL" />
+        <Button onClick={handleButtonClick} style={{ marginTop: "12px", marginBottom: "12px" }} variant="contained" >Shorten URL</Button>
+        {shortURL && <Link href={`${myHost}/${shortURL}`} style={{ textAlign: "center" }} rel="noreferrer" target="_blank" >{`${myHost}/${shortURL}`}</Link>}
+        {error && <Alert style={{ textAlign: "center" }} severity="error">URL is not valid</Alert>}
+      </FormGroup>
+    </Box></div>);
+  }
 
 export default URLShortener;
