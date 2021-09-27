f27689570ef37c52549363909c11ca45af0c71bd
pastepi <pawel.st123@gmail.com>
2021-09-27 20:07:40 +0200

    Adding material UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,8 @@
 import URLShortener from './URLShortener';
-import './App.css'
+
 
 const App = () => {
-  return <URLShortener 
-    containerStyle= { {  } }
-    formStyle=      { {  } }
-    buttonStyle=    { { border: "solid 2px black", borderRadius: "3px", padding: "5px" } }
-    inputStyle=     { {color: "rgb(225, 225, 225)", border: "none", background: "rgb(146, 147, 148)", outline: "solid 2px grey",
-                      padding: "8px", borderRadius: "5px", fontWeight: "600", caretShape: "block", textShadow: "1px 1px 2px black"}}
-    errorStyle=     { { color: "#fff", background: "rgb(227, 32, 32)", padding: "10px", borderRadius: "5px", fontWeight: "600"} }
-    linkStyle=      { { color: "#000" } }
+  return <URLShortener useMaterialUI
   />
 
 }
--- frontend/src/URLShortener.js
+++ frontend/src/URLShortener.js
@@ -1,39 +1,41 @@
 import { useState } from "react"
+import { Input, Button, Link, Alert, FormGroup, Box } from '@mui/material'
 
 
-const defaultContainerStyle = {minHeight: "150px", margin: "0", minWidth: "300px", display: "flex", alignItems: "center", justifyContent: "center"};
-const defaultFormStyle = {display: "flex", flexDirection: "column", alignItems: "center", minHeight: "150px", justifyContent: "space-around"};
-const defaultButtonStyle = {fontSize: "1rem"};
-const defaultInputStyle = {fontSize: "1rem"};
-const defaultLinkStyle = {fontSize: "1.2rem"};
-const defaultErrorStyle = {color: "red"};
+const defaultContainerStyle = { minHeight: "150px", margin: "0", minWidth: "300px", display: "flex", alignItems: "center", justifyContent: "center" };
+const defaultFormStyle = { display: "flex", flexDirection: "column", alignItems: "center", minHeight: "150px", justifyContent: "space-around" };
+const defaultButtonStyle = { fontSize: "1rem" };
+const defaultInputStyle = { fontSize: "1rem" };
+const defaultLinkStyle = { fontSize: "1.2rem" };
+const defaultErrorStyle = { color: "red" };
 
 
 const myHost = "http://localhost:8080";
 
 
-const URLShortener = ({ containerStyle, formStyle, buttonStyle, inputStyle, linkStyle, errorStyle }) => {
-    
-    const [url, setURL] = useState("");
-    const [shortURL, setShortURL] = useState(null);
-    const [error, setErrorState] = useState(false);
-  
-    const handleURLChange = (e) => {
-      setURL(e.target.value)
-    }
-  
-    const isValidURL = (srcURL) => {
-      let pattern = new RegExp('^(https?:\\/\\/)?'+         // protocol
-        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
-        '((\\d{1,3}\\.){3}\\d{1,3}))'+                      // OR ip (v4) address
-        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+                  // port and path
-        '(\\?[;&a-z\\d%_.~+=-]*)?'+                         // query string
-        '(\\#[-a-z\\d_]*)?$','i');                          // fragment locator
-    
-        return pattern.test(srcURL);
-    }
+const URLShortener = ({ containerStyle, formStyle, buttonStyle, inputStyle, linkStyle, errorStyle, useMaterialUI }) => {
 
-    const handleButtonClick = (e) => {
+  const [url, setURL] = useState("");
+  const [shortURL, setShortURL] = useState(null);
+  const [error, setErrorState] = useState(false);
+
+  const handleURLChange = (e) => {
+    setURL(e.target.value)
+  }
+
+  const isValidURL = (srcURL) => {
+    let pattern = new RegExp('^(https?:\\/\\/)?' +         // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))' +                      // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +                  // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?' +                         // query string
+      '(\\#[-a-z\\d_]*)?$', 'i');                          // fragment locator
+
+    return pattern.test(srcURL);
+  }
+
+  const handleButtonClick = (e) => {
+    if (e.keyCode === 13 || e.keyCode === undefined) {
       e.preventDefault()
       if (error) {
         setErrorState(false)
@@ -52,33 +54,42 @@ const URLShortener = ({ containerStyle, formStyle, buttonStyle, inputStyle, link
         }
 
         fetch(myHost + "/URL", {
-          method: "POST", 
+          method: "POST",
           headers: {
-          "Content-Type": "application/json"
+            "Content-Type": "application/json"
           },
-          body: JSON.stringify({url:srcURL})
-      }).then(response => response.json()
-      ).then(data => setShortURL(data.ShortURL))
+          body: JSON.stringify({ url: srcURL })
+        }).then(response => response.json()
+        ).then(data => setShortURL(data.ShortURL))
       } else {
         setErrorState(true);
         setShortURL(null);
       }
     }
+  }
 
-    const styleElements = (defStyle, style) => {
-      return style ? {...defStyle, ...style} : defStyle
-    }
-    
-    return (
-      <div style={ styleElements(defaultContainerStyle, containerStyle) }>
-        <form style={ styleElements(defaultFormStyle, formStyle) }>
-          <input          style={ styleElements(defaultInputStyle, inputStyle) } type="text" value={url} onChange={handleURLChange} spellCheck="false" placeholder="Your URL"/>
-          <button         style={ styleElements(defaultButtonStyle, buttonStyle) } onClick={handleButtonClick}>Shorten URL</button>
-          {shortURL && <a style={ styleElements(defaultLinkStyle, linkStyle) } href={`${myHost}/${shortURL}`} rel="noreferrer" target="_blank" >{`${myHost}/${shortURL}`}</a>}
-          {error && <div  style={ styleElements(defaultErrorStyle, errorStyle) }>URL is not valid</div>}
-        </form>
-      </div>
-    );
+  const styleElements = (defStyle, style) => {
+    return style ? { ...defStyle, ...style } : defStyle
+  }
+
+  return !useMaterialUI ? (
+    <div style={styleElements(defaultContainerStyle, containerStyle)}>
+      <form style={styleElements(defaultFormStyle, formStyle)}>
+        <input style={styleElements(defaultInputStyle, inputStyle)} type="text" value={url} onChange={handleURLChange} spellCheck="false" placeholder="Your URL" />
+        <button style={styleElements(defaultButtonStyle, buttonStyle)} onClick={handleButtonClick}>Shorten URL</button>
+        {shortURL && <a style={styleElements(defaultLinkStyle, linkStyle)} href={`${myHost}/${shortURL}`} rel="noreferrer" target="_blank" >{`${myHost}/${shortURL}`}</a>}
+        {error && <div style={styleElements(defaultErrorStyle, errorStyle)}>URL is not valid</div>}
+      </form>
+    </div>
+  ) : (<div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+    <Box sx={{ border: 1 }} style={{ width: "80%", maxWidth: "500px", padding: "16px", borderRadius: "5px", border: "2px solid #1976d2" }} >
+      <FormGroup>
+        <Input type="text" onKeyDown={handleButtonClick} value={url} onChange={handleURLChange} spellCheck="false" placeholder="Your URL" />
+        <Button onClick={handleButtonClick} style={{ marginTop: "12px", marginBottom: "12px" }} variant="contained" >Shorten URL</Button>
+        {shortURL && <Link href={`${myHost}/${shortURL}`} style={{ textAlign: "center" }} rel="noreferrer" target="_blank" >{`${myHost}/${shortURL}`}</Link>}
+        {error && <Alert style={{ textAlign: "center" }} severity="error">URL is not valid</Alert>}
+      </FormGroup>
+    </Box></div>);
 }
 
 export default URLShortener;
